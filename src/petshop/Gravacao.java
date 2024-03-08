80cfb00092b473bda942cdce61c717b239fc5764
Cassiano Castro <development.castro@gmail.com>
2024-03-08 00:58:32 -0300

    Refactor: renamed local vars and moved private static methods to File{Dialog,Manipulator} classes

--- src/petshop/Gravacao.java
+++ src/petshop/Gravacao.java
@@ -1,8 +1,8 @@
 package petshop;
 
-import javax.swing.*;
-import java.io.*;
-import java.util.*;
+import javax.swing.JOptionPane;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  *
@@ -15,63 +15,22 @@ public class Gravacao
      */
     static public void main(String[] args)
     {
-        File file;
-        Object object;
-        JFileChooser chooser = new JFileChooser();
-
-        String name   = JOptionPane.showInputDialog(null, "Nome: ");
-        String weight = JOptionPane.showInputDialog(null, "Tipo: ");
-
-        Racao racao = new Racao(name, Double.parseDouble(weight));
-
         List<Racao> list = new ArrayList<>();
-        list.add(racao);
+        
+        String name   = JOptionPane.showInputDialog(null, "Name: ");
+        String weight = JOptionPane.showInputDialog(null, "Weight: ");
 
-        if ( chooser.showSaveDialog(null) == JFileChooser.APPROVE_OPTION )
+        list.add(new Racao(name, Double.parseDouble(weight)));
+        
+        var dialog = new FileDialog();
+        
+        dialog.save(list);
+        
+        list = dialog.recovery();
+        
+        for ( final Racao r : list )
         {
-            file = chooser.getSelectedFile();
-
-            try (ObjectOutputStream output
-                = new ObjectOutputStream(
-                    new FileOutputStream(file)))
-            {
-                output.writeObject(list);
-            }
-            catch ( IOException e )
-            {
-                JOptionPane.showMessageDialog(null, e.getMessage());
-            }
-        }
-	else if ( chooser.showSaveDialog(null) == JFileChooser.CANCEL_OPTION )
-        {
-            JOptionPane.showMessageDialog(null, "Op. cancelada");
-        }
-
-        if ( chooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION )
-        {
-            file = chooser.getSelectedFile();
-            List<Racao> list2 = new ArrayList();
-
-            try (ObjectInputStream input
-                = new ObjectInputStream(
-                    new FileInputStream(file)))
-            {
-                object = input.readObject();
-                list2  = (List<Racao>) object;
-            }
-            catch ( ClassNotFoundException | IOException e )
-            {
-                JOptionPane.showMessageDialog(null, e.getMessage());
-            }
-
-            for ( final Racao r : list2 )
-            {
-                JOptionPane.showMessageDialog(null, r.toString());
-            }
-        }
-	else if ( chooser.showOpenDialog(null) == JFileChooser.CANCEL_OPTION )
-        {
-            JOptionPane.showMessageDialog(null, "Op. cancelada");
+            JOptionPane.showMessageDialog(null, r.toString());
         }
     }
 }
